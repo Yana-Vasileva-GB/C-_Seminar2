aa3909c4a6e6c6fe073f854576d287336f494cc3
Yana <jana-ivanova@mail.ru>
2022-08-08 23:47:44 +0400

    Добавили файл .gitignore

---
+++ Example1_If_Else/Program.cs
@@ -0,0 +1,11 @@
+﻿int temp = -30;
+
+/* 
+else if - иначе если, ищет первую истину и на этом завершает проверку условий.
+if - новая проверка, 4 отдельные проверки
+*/
+
+if (temp < 0) Console.WriteLine("Надень шапку");
+else if (temp < -10) Console.WriteLine("Надень зимнюю куртку");
+else if (temp < -20) Console.WriteLine("Лучше остаться дома");
+else if (temp <= -30) Console.WriteLine("Сидим дома");
