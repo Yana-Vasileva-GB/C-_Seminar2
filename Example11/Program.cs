aef00eb099e32d2566681405833eea79617bf0de
Yana <jana-ivanova@mail.ru>
2022-08-09 23:19:44 +0400

    Решение задачи 11, Правки 3

--- Example11/Program.cs
+++ Example11/Program.cs
@@ -8,7 +8,8 @@
 
 /* 
 1 ВАРИАНТ:
-int number = new Random().Next(100,999);
+
+int number = new Random().Next(100,1000); // (100;999]
 Console.WriteLine($"Рандомное число: {number}");
 int firstDigit = number / 100;
 int threeDigit = number % 10;
@@ -16,8 +17,15 @@ Console.WriteLine($"Число без второй цифры - {firstDigit}{thr
 */
 
 /* 
-2 ВАРИАНТ:
-*/
+2 ВАРИАНТ:*/
 
 string inputNumber = Console.ReadLine(); // 456
 Console.WriteLine(inputNumber[0]+inputNumber[2]);
+
+/* 
+3 ВАРИАНТ:
+
+int number = new Random().Next(100,1000); // (100;999]
+int result = ((number / 100) * 10) + number % 10; // 456 -> 4, 456 % 10 -> 6
+Console.WriteLine($"Исходное число: {number}, результат: {result}");
+*/
