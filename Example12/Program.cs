0440fa29047558e141fed089a7b14e7beaf430d2
Yana <jana-ivanova@mail.ru>
2022-08-10 11:24:21 +0400

    Решение задачи 12, правки

--- Example12/Program.cs
+++ Example12/Program.cs
@@ -1,7 +1,7 @@
 ﻿/*
 Задача 12. 
 Напишите программу, которая будет принимать на вход два числа и выводить, 
-является ли второе число кратным первому. Если число 2 не кратно числу 1, 
+является ли первое число кратным второму. Если число 1 не кратно числу 2, 
 то программа выводит остаток от деления.
 34, 5 -> не кратно, остаток 4 
 16, 4 -> кратно
@@ -11,12 +11,11 @@ Console.WriteLine("Введите два числа");
 int firstNumber = Convert.ToInt32(Console.ReadLine());
 int secondNumber = Convert.ToInt32(Console.ReadLine());
 
-if (secondNumber % firstNumber == 0)
+if (firstNumber % secondNumber == 0)
 {
     Console.WriteLine("Второе число кратно первому");    
 }
 else
 {
-    int remainderNumber = secondNumber % firstNumber;
-    Console.WriteLine($"Второе число НЕ кратно первому. Остаток от деления = {remainderNumber}");
+    Console.WriteLine($"Второе число НЕ кратно первому. Остаток от деления = {firstNumber % secondNumber}");
 }
